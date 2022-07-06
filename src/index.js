63725c2cbe9d6efba2829a5502925e275afdb10a
Arno-Pm <70451630+Arno-Pm@users.noreply.github.com>
2022-07-06 13:12:31 +0300

    fourth try

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ export const initConnection = () => {
   const client = new Client({
     user: POSTGRES_USER || 'postgres',
     host: POSTGRES_HOST || 'localhost',
-    database: POSTGRES_DB || 'postgres',
-    password: POSTGRES_PASSWORD || 'postgres',
-    port: POSTGRES_PORT || 5556,
+    database: POSTGRES_DB || 'dbtask',
+    password: POSTGRES_PASSWORD || '567RTY89001a',
+    port: POSTGRES_PORT || 5432,
   });
 
   return client;
@@ -22,19 +22,57 @@ export const initConnection = () => {
 export const createStructure = async () => {
   const client = initConnection();
   client.connect();
-
-  // Your code is here...
-  // Your code is here...
-
+  await client.query(`CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    date DATE NOT NULL DEFAULT CURRENT_DATE
+  );`);
+  await client.query(`CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+  );`);
+  await client.query(`CREATE TABLE authors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+  );`);
+  await client.query(`CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    userid INTEGER NOT NULL,
+    authorid INTEGER NOT NULL,
+    categoryid INTEGER NOT NULL,
+    FOREIGN KEY (userid) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (authorid) REFERENCES authors (id) ON DELETE CASCADE,
+    FOREIGN KEY  (categoryid) REFERENCES categories (id) ON DELETE CASCADE
+  );`);
+  await client.query(`CREATE TABLE descriptions (
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(10000) NOT NULL,
+    bookid INT UNIQUE NOT NULL,
+    FOREIGN KEY (bookid) REFERENCES books (id) ON DELETE CASCADE
+  );`);
+  await client.query(`CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    message VARCHAR(10000) NOT NULL,
+    userid INT NOT NULL,
+    bookid INT NOT NULL,
+    FOREIGN KEY (userid) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (bookid) REFERENCES books (id) ON DELETE CASCADE
+  );`)
   client.end();
 };
+
 
 export const createItems = async () => {
   const client = initConnection();
   client.connect();
-
-  // Your code is here...
-
+  // Богдан сори, небольшой спам в базе ,  по ошибке создал три id в бд, а так все рабоатет) 
+  await client.query(`INSERT INTO users(name) VALUES('Egor ');`);
+  await client.query(`INSERT INTO categories(name) VALUES('IT sphere');`);
+  await client.query(`INSERT INTO authors(name) VALUES('Egor Gostryck');`);
+  await client.query(`INSERT INTO books(title, userid, authorid, categoryid) VALUES('Book about success', 1, 1, 1);`);
+  await client.query(`INSERT INTO descriptions(description, bookid) VALUES('Shut up and take my money', 1);`);
+  await client.query(`INSERT INTO reviews(name) VALUES('Egor ', 1);`);
   client.end();
 };
 
